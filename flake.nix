e63bc0e8c86d4aadb9dca657053775a068583fcc
Mona Mayrhofer <mona.mayrhofer@proton.me>
2023-08-25 21:43:00 +0200

    nix and style fix

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Build a NPM Project";
+
+  inputs = {
+    nixpkgs = {
+      url = "nixpkgs/nixos-22.11";
+    };
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+
+      buildNodeJs = pkgs.callPackage "${nixpkgs}/pkgs/development/web/nodejs/nodejs.nix" {
+        python = pkgs.python3;
+      };
+
+      nodejs = buildNodeJs {
+        enableNpm = true;
+        version = "18.17.1";
+        sha256 = "sha256-8hXPA9DwDwesC2dMaBn4BMFULhbxUtoEmAAirsz15lo=";
+      };
+    in rec {
+      flakedPkgs = pkgs;
+
+      # enables use of `nix shell`
+      devShell = pkgs.mkShell {
+        # add things you want in your shell here
+        buildInputs = with pkgs; [
+          nodejs
+        ];
+      };
+    });
+}
